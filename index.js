096bd65ecb831095e49bcac8e3d1ca6c19180054
William Warner Burke <williamburke121@gmail.com>
2022-01-07 00:40:57 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+
+main.remove(main);
+const newHeader = document.createElement("h1");
+newHeader.id = 'victory';
+newHeader.innerHTML = "Will is the champion";
+document.body.append(newHeader);
